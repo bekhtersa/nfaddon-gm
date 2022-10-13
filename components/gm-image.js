3cb580dbcc1a143d6405c41e765084b14196b85c
Bekhter Sergey <sabgrand@gmail.com>
2022-10-13 18:29:49 +0300

    Add bg property

--- components/gm-image.js
+++ components/gm-image.js
@@ -19,23 +19,33 @@ class GMImage extends PlElement {
         storage: {
             type: String,
             value: 'nfcfiles'
+        },
+        bg: {
+            tyep: Boolean,
+            value: false
         }
     }
 
     static css = css`
         :host{
             display: flex;
+            background-size: cover;
         }
         img {
             width: 100%;
         }
+        img[hidden] {
+            display: none;
+        }
     `;
     static template = html`
-        <img src="[[url(filename, unit, size, storage)]]" />
+        <img src="[[url(filename, unit, size, storage)]]" hidden$="[[bg]]"/>
     `;
 
     url(filename,unit,size,storage) {
-        return filename?`${host}/@${storage}/view/${ (unit && size) ? `${unit}.${size}.` : '' }${filename}`:'';  
+        const url = filename?`${host}/@${storage}/view/${ (unit && size) ? `${unit}.${size}.` : '' }${filename}`:'';
+        this.style.backgroundImage = `url('${url}')`;
+        return url;
     }
 }
 
